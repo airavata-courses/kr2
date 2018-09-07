91759aefcf96389b5949c8b49652b1e8d0ccd699
rdasego <rdasego@iu.edu>
2018-09-07 15:51:21 -0400

    Initial page to diaply user name input

---
+++ ReactUI/usnaukri/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+import UserProfile from "./UserProfile";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <UserProfile />
+      </div>
+    );
+  }
+}
+
+export default App;
