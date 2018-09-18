26d5f64522e09a86b29c2fda8cca337f4200d1e4
karanrk <karankamatgi@gmail.com>
2018-09-18 03:04:41 -0400

    added more tests

--- test/test_server.py
+++ test/test_server.py
@@ -12,17 +12,20 @@ def test_initial():
     print(init)
     assert b"US Naukri" in init.data
 
-'''
+
 def test_job():
 
     # Test if we are able to get jobs
     test_app = server.app
     test_client = test_app.test_client()
     test_app.testing = True
-    jobs = test_client.get('/jobs/total')
-    data = json.loads(news.get_data(as_text=True))
-    assert data["success"] == 1
-    	
+    headers_content_form_data = {'Content-Type': 'multipart/form-data'}
+    jobs = test_client.post('/jobs/total',headers=TestClass.headers_content_form_data,
+        data={'desc': 'node','jobType':'true'})
+    data = json.loads(jobs.get_data(as_text=True))
+    #assert data["success"] == 1
+    assert jobs.status_code == 200
+'''    	
     # Test if we are able to get error when token is wrong
     test_app = app.flask_news("wrong_token")
     test_client = test_app.test_client()
