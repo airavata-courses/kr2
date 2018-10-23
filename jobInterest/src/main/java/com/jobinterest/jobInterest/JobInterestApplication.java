76a1b3ed075e399282f023fe9ad67fea2d79c2d9
rdasego <rdasego@iu.edu>
2018-10-22 20:51:11 -0400

    Initial project setup for SpringBoot

---
+++ jobInterest/src/main/java/com/jobinterest/jobInterest/JobInterestApplication.java
@@ -0,0 +1,12 @@
+package com.jobinterest.jobInterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobInterestApplication.class, args);
+	}
+}
