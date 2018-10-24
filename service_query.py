34b8e31299502ee83141a032ffa81e4750ab91da
Raghottam Talwai <raghottam.talwai@gmail.com>
2018-10-24 04:19:05 -0400

    Changed query code

--- service_query.py
+++ service_query.py
@@ -1,10 +1,10 @@
 from flask import Flask
-import requests
 import json
 from flask.json import jsonify
 from flask import render_template, request
 from flask_cors import CORS, cross_origin
-import zc.zk
+# import zc.zk
+from kazoo.client import KazooClient
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -13,18 +13,16 @@ CORS(app, support_credentials=True)
 @cross_origin(supports_credentials=True)
 
 def service_query():
-    zk = zc.zk.ZooKeeper('149.165.170.124:2182')
-    url_java = zk.children('/usNaukri/java')
-    url_flask = zk.children('/usNaukri/flask')
-    url_node = zk.children('/usNaukri/node')
-    url_java=['149.165.170.240:9090']
-    url_flask=['149.165.170.151:5000']
-    url_node=['149.165.170.57:4000']
-    # print sorted(url_java),sorted(url_flask),sorted(url_node)
 
-    return jsonify({"flask":url_flask[0],"java":url_java[0],"node":url_node[0]})
+    zk = KazooClient(hosts='149.165.170.124:2182')
+    zk.start()
+    url_flask,stat = zk.get("/usNaukri/flask")
+    url_java ,stat = zk.get("/usNaukri/java")
+    url_node ,stat = zk.get("/usNaukri/node")
+    # print url_flask,url_java,url_node
+    return jsonify({"flask":url_flask,"java":url_java,"node":url_node})
     # return "ok"
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000)
-   # app.run()
+   # app.run(host='0.0.0.0', port=5000)
+   app.run()
