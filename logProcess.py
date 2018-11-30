fcefaf13759e98503dd6677f0e0a597b4a012741
karanrk <karankamatgi@gmail.com>
2018-11-30 00:46:48 -0500

    adding raw log processing

---
+++ logProcess.py
@@ -0,0 +1,24 @@
+#File to read the raw airavata debug logs, process and write them to processed.log
+
+#memory map useful for easier pattern matching, reading and writing data starting at different position using seek
+import mmap
+
+with open('airavata.log', 'r') as raw_log: #api dev server raw logs
+    
+    # size 0 means whole file, prot for Unix version
+    mmap = mmap.mmap(raw_log.fileno(), 0, prot=mmap.PROT_READ)  
+                          
+
+    startIndex = mmap.rfind('Experiment Created') # search for last occurrence of 'string'
+    				   
+    mmap.seek(startIndex)             # seek to the location
+    # line = mmap.readline()
+       				
+    
+    line=mmap.readline()
+    with open('processed.log', 'a') as processed_log:
+    	while (line):  
+    		# print(line)
+    		processed_log.write(line)
+    		line=mmap.readline()
+    
